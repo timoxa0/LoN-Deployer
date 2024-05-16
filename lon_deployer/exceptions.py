fdad3b80b27c3ac6cd6e2bc52eaae31c6cb68bdc
timoxa0 <tima012007@gmail.com>
2024-05-16 23:27:44 +0500

    Add UnauthorizedBootImage and UnsupportedPlatform exceptions

--- lon_deployer/exceptions.py
+++ lon_deployer/exceptions.py
@@ -1,4 +1,10 @@
 class FastbootException(Exception):
+    def __init__(self, message, output):
+        super().__init__(message)
+        self.output = output
+
+
+class UnauthorizedBootImage(FastbootException):
     pass
 
 
@@ -8,3 +14,9 @@ class DeviceNotFound(Exception):
 
 class RepartitonError(Exception):
     pass
+
+
+class UnsupportedPlatform(Exception):
+    def __init__(self, platform):
+        super().__init__(f"{platform} is not supported")
+        self.platform = platform
